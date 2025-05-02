993c379fe6890941b5f3d56de49b16daecbd2eab
RishabhKumar2901 <rishaabkumar09@gmail.com>
2025-05-02 14:31:07 +0530

    section 3 fix

--- src/pages/VehicleCareBenifits.tsx
+++ src/pages/VehicleCareBenifits.tsx
@@ -1,40 +1,76 @@
-import benifit1 from "../assets/benifit1.png";
 import tick_icon from "../assets/tick_icon.svg";
 import cross_icon from "../assets/cross_icon.svg";
 import tick_icon_red from "../assets/tick_icon_red.svg";
+import { vehicleCareBenifitsData } from "../static/vehicleCareBenifitsData";
 
 const VehicleCareBenifits = () => {
   return (
     <div className="flex flex-col items-center font-montserrat">
-      <div className="flex space-x-5 items-center text-center">
-        <div className="text-3xl font-[700] flex flex-wrap w-1/4 mt-6 text-[#282c30] px-8">
-          Vehicle Care Benifits
+      <div className="flex items-center text-center w-3/4 justify-center ml-2">
+        <div className="text-3xl font-[700] text-[#282c30] flex items-center border-[1px] border-[#f9f9fb] py-3 px-8 w-5/6 justify-center">
+          Vehicle Care <br /> Benifits
         </div>
-        <div className="text-lg font-bold bg-[#6c747c] py-5 px-8 text-white">
-          Local Mechanic
+        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f9f9fb] pt-3 pb-4 px-2 w-4/12">
+          <div className="text-lg font-bold bg-[#6c747c] py-5 px-[3.7rem] text-nowrap text-white">
+            Local Mechanic
+          </div>
         </div>
-        <div className="text-lg font-bold bg-[#6c747c] py-5 px-8 text-white">
-          Authorized Service Center
+        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f9f9fb] pt-3 pb-4 px-2 w-2/5">
+          <div className="text-lg font-bold bg-[#6c747c] py-5 px-[3.42rem] text-white text-nowrap">
+            Authorized Service Center
+          </div>
         </div>
-        <div className="text-lg font-bold bg-[#ed1d25] py-5 px-8 text-white">
-          Vehiclecare
-        </div>
-      </div>
-      <div className="flex items-center text-center pr-16">
-        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] py-3 px-16">
-          <img src={benifit1} alt="Benifit" className="w-5 h-5" />
-          &nbsp;Free pick up and drop
-        </div>
-        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] py-2.5 px-28">
-          <img src={tick_icon} alt="Benifit" className="w-6 h-7" />
-        </div>
-        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] py-3.5 px-40">
-          <img src={cross_icon} alt="Benifit" className="w-6 h-5" />
-        </div>
-        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] py-2.5 px-28">
-          <img src={tick_icon_red} alt="Benifit" className="w-6 h-7" />
+        <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f9f9fb] pt-3 pb-4 px-2 w-1/4">
+          <div className="text-lg font-bold bg-[#ed1d25] py-5 px-[3.89rem] text-white text-nowrap">
+            Vehiclecare
+          </div>
         </div>
       </div>
+
+      {vehicleCareBenifitsData?.map((item, index) => (
+        <div
+          className="flex items-center text-center w-3/4 justify-center ml-2"
+          key={index}
+        >
+          <div className="text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] py-3 px-8 w-5/6 justify-center">
+            <img src={item?.imageUrl} alt="Benifit" className="w-5 h-5" />
+            &nbsp;{item?.benifit}
+          </div>
+          <div
+            className={`text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] ${
+              item?.localMechanic ? "py-2.5" : "py-3.5"
+            } px-28 w-4/12`}
+          >
+            <img
+              src={item?.localMechanic ? tick_icon : cross_icon}
+              alt="Benifit"
+              className={item?.localMechanic ? "w-6 h-7" : "w-6 h-5"}
+            />
+          </div>
+          <div
+            className={`text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] ${
+              item?.authoriedServiceCenter ? "py-2.5" : "py-3.5"
+            } px-40 w-2/5`}
+          >
+            <img
+              src={item?.authoriedServiceCenter ? tick_icon : cross_icon}
+              alt="Benifit"
+              className={item?.authoriedServiceCenter ? "w-6 h-7" : "w-6 h-5"}
+            />
+          </div>
+          <div
+            className={`text-base font-[700] text-[#282c30] flex items-center border-[1px] border-[#f1f3f2] ${
+              item?.vehicleCare ? "py-2.5" : undefined
+            } px-28 w-1/4`}
+          >
+            <img
+              src={item?.vehicleCare ? tick_icon_red : undefined}
+              alt="Benifit"
+              className={item?.vehicleCare ? "w-6 h-7" : undefined}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/static/vehicleCareBenifitsData.tsx
@@ -0,0 +1,59 @@
+import benifit1 from "../assets/benifit1.png";
+import benifit2 from "../assets/benifit2.png";
+import benifit3 from "../assets/benifit3.png";
+import benifit4 from "../assets/benifit4.png";
+import benifit5 from "../assets/benifit5.png";
+import benifit6 from "../assets/benifit6.png";
+import benifit7 from "../assets/benifit7.png";
+
+export const vehicleCareBenifitsData = [
+    {
+        benifit: "Free pick up and drop",
+        imageUrl: benifit1,
+        localMechanic: true,
+        authoriedServiceCenter: false,
+        vehicleCare: true
+    },
+    {
+        benifit: "40 point check up",
+        imageUrl: benifit2,
+        localMechanic: false,
+        authoriedServiceCenter: false,
+        vehicleCare: true
+    },
+    {
+        benifit: "3 months warranty",
+        imageUrl: benifit3,
+        localMechanic: false,
+        authoriedServiceCenter: true,
+        vehicleCare: true
+    },
+    {
+        benifit: "Genuine spares",
+        imageUrl: benifit4,
+        localMechanic: false,
+        authoriedServiceCenter: true,
+        vehicleCare: true
+    },
+    {
+        benifit: "Doorstep Service",
+        imageUrl: benifit5,
+        localMechanic: false,
+        authoriedServiceCenter: true,
+        vehicleCare: true
+    },
+    {
+        benifit: "Expert mechanics",
+        imageUrl: benifit6,
+        localMechanic: false,
+        authoriedServiceCenter: true,
+        vehicleCare: true
+    },
+    {
+        benifit: "Same day delivery",
+        imageUrl: benifit7,
+        localMechanic: false,
+        authoriedServiceCenter: false,
+        vehicleCare: true
+    },
+]
